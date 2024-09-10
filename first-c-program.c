027c50a02c8b0d1c1764f53a7974ad24586eb645
SCS Balaji <150114634+SCSBalaji@users.noreply.github.com>
2024-09-10 14:41:12 +0530

    updated many line in the first-c-program.c file

--- first-c-program.c
+++ first-c-program.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World\n");
+    printf("Hello World!🌐\n");
     printf("This is my first C program in GitHub\t");
     printf("From Let us C book\t\t\t");
-    printf("19 Edition");
+    printf("19 Edition\n");
+    printf("I will upload all the codes from the book\n\n");
+    printf("Not by just copying and pasting");
+    printf("\nBy understanding the code step by step and explain it properly with comment lines");
     return 0;
 }
