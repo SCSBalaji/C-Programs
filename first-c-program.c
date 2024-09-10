b3db2f120148e00fa141221dee1f53db2fe506e2
SCS Balaji <150114634+SCSBalaji@users.noreply.github.com>
2024-09-10 14:29:00 +0530

    create first-c-program.c file

---
+++ first-c-program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("This is my first C program in GitHub\t");
+    printf("From Let us C book\t\t\t");
+    printf("19 Edition");
+    return 0;
+}
