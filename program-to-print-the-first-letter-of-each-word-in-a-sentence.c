2dc8f0bb8ba6b02cec9b8f20e2357bc925a74d63
SCS Balaji <150114634+SCSBalaji@users.noreply.github.com>
2024-09-12 14:09:05 +0530

    Create program-to-print-the-first-letter-of-each-word-in-a-sentence.c  file
    program-to-print-the-first-letter-of-each-word-in-a-sentence.c file and its output file

---
+++ program-to-print-the-first-letter-of-each-word-in-a-sentence.c
@@ -0,0 +1,23 @@
+/* Program to Print the First Letter of Each Word in a Sentence */
+
+#include<stdio.h>
+#include<string.h>      // for string functions
+int main()
+{
+    char a[100];     
+    int i;
+    printf("Enter a Sentence :\n");
+    gets(a);            /* Because we are taking string as input. If we take scanf it will take only till the first blank space called NULL character only. It leaves all the remaining sentence.*/
+    for ( i = 0; i < strlen(a); i++)        // Loop through the entire string
+    {
+        if (i == 0 && a[i] != ' ')          // Check if the first character is not a space, if not a space then print it
+        {
+            printf("%c ",a[i]);
+        }
+        else if (i > 0 && a[i] == ' ')      // Check if the current character is a space or not, if it is a space then print the next character after the space
+        {
+            printf("%c ",a[i + 1]);
+        }
+    }
+    return 0;
+}
